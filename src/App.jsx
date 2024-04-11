9f4fdef8559029f10ef980a90d93fa07b559c49f
Aniket Chaudhary <iamani7464@gmail.com>
2024-04-12 00:28:33 +0530

    Fixed base url

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import DesignOne from './DesignOne';
 import DesignTwo from './DesignTwo';
 import DesignThree from './DesignThree';
 import ImageCard from './ImageCard';
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   var [a, b] = useState(69);
   return (
     <>
       <Navbar name="Hello"/>
-      <Routes>
-        <Route path="/" element={<DesignOne />} />
-        <Route path="/DesignTwo" element={<DesignTwo />} />
-        <Route path="/DesignThree" element={<DesignThree />} />
-        <Route path="/DesignFour" element={<ImageCard />} />
-      </Routes>
+      <Router basename="/nippple">
+        <Routes>
+          <Route path="/" element={<DesignOne />} />
+          <Route path="/DesignTwo" element={<DesignTwo />} />
+          <Route path="/DesignThree" element={<DesignThree />} />
+          <Route path="/DesignFour" element={<ImageCard />} />
+        </Routes>
+      </Router>
     </>
   )
 }
